fb7cb13f12c4f2eabd0a0dcbc0934d3c9892375b
Jimena <mjimevm@gmail.com>
2026-05-11 12:00:37 -0600

    Defined Structs and Mutex initialization

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+/*
+*-----------------------------------------------------------
+* main.cpp
+*-----------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* FACULTAD DE INGENIERÍA
+* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
+*
+* CC3086 - Programacion de Microprocesadores
+*
+*-----------------------------------------------------------
+* Descripción: Juego de Bomberman
+*-----------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+#include <pthread.h>
+#include <windows.h>
+
+pthread_mutex_t mutex;
+
+struct Jugador {
+    std::string name;
+    int vida;
+    int x;
+    int y;
+    int cantidadBombas;
+    std::vector<std::pair<int, int>> bombas; // Vector para almacenar las posiciones
+};
+
+struct Enemigo {
+    std::string name;
+    int vida;
+    int x;
+    int y;
+};
+
+struct Mapa {
+    int alto;
+    int largo;
+    char** mapa;
+}
+
+int main() {
+    // Inicializar mutex
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
