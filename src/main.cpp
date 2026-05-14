e5ad43ecfd7266150a43f3a93c863d33005a93ab
Jimena <mjimevm@gmail.com>
2026-05-14 16:11:24 -0600

    Se agregó el modo de Un Jugador y se arreglaron pequeños bugs al implementarlo, además se le resta la vida cada vez que explota la bomba

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ uniform_int_distribution<> rand1to9(1, 9);
 
 struct Jugador {
     string name;
-    int vida;
+    int vidas;
     int x;
     int y;
     int cantidad;
@@ -42,7 +42,7 @@ struct Jugador {
 
 struct Enemigo {
     string name;
-    int vida;
+    int vidas;
     int x;
     int y;
 };
@@ -57,12 +57,18 @@ struct Bomba {
     int distancia; 
     int x;
     int y;
+    int autor; // para saber quien la coloco
 };
 
 struct Muro {
     int x;
     int y;
     bool destructible;
+};
+struct Puerta {
+    int x;
+    int y;
+    bool abierta;
 };
 
 struct Juego {
@@ -71,7 +77,13 @@ struct Juego {
     vector<Enemigo> enemigos;
     vector<Bomba> bombas;
     vector<Muro> muros;
+    
+    // Modo un jugador
+    int nivel = 1;
+    int puntaje = 0;
+    Puerta puerta = {0, 0, false};
 };
+
 
 //Estructura para enviar los datos de la bomba al hilo
 struct Explosion {
@@ -79,12 +91,21 @@ struct Explosion {
     Bomba bomba;
 };
 
-void inicializarJugadores(Juego &j) {
-    Jugador p1 = {"Player1", 3, 1, 1, 1};
-    Jugador p2 = {"Player2", 3, 29, 13, 1};
+void inicializarJugadores(Juego &j, bool modoUnJugador) {
+    j.jugadores.clear();
+    if (modoUnJugador) {
+        Jugador p1 = {"Player1", 3, 1, 1, 0};
+        j.jugadores.push_back(p1);
+        return;
+    }
+    if (!modoUnJugador) {
+        Jugador p1 = {"Player1", 3, 1, 1, 0};
+        Jugador p2 = {"Player2", 3, 29, 13, 0};
 
-    j.jugadores.push_back(p1);
-    j.jugadores.push_back(p2);
+        j.jugadores.push_back(p1);
+        j.jugadores.push_back(p2);
+        return;
+    }
 };
 
 //Funcion de lugar de inicio de jugadores
@@ -138,7 +159,22 @@ void inicializarMapa(Juego &j) {
     }
 };
 
-void dibujarMapa(const Juego &j) {
+void interfaz_un_jugador (const Juego &j, bool modoUnJugador) {
+    int uiy = j.mapa.alto + 1;
+    if (modoUnJugador) {
+        mvprintw(uiy, 0, "Nivel: %d  Puntaje: %d  Vidas: %d   (E) bomba  (Y) salir", 
+            j.nivel, j.puntaje, j.jugadores[0].vidas);
+        if (j.puerta.abierta) {
+            mvprintw(uiy + 1, 0, "Puerta: (%d,%d) - Cruza la puerta (X) para pasar de nivel", j.puerta.x, j.puerta.y);
+        } else {
+            mvprintw(uiy + 1, 0, "Puerta cerrada - Destruye muros para encontrar la puerta");
+        }
+    } else {
+        mvprintw(uiy, 0, "J1 Vidas: %d  Puntaje: %d     J2 Vidas: %d  Puntaje: %d", j.jugadores[0].vidas, j.jugadores[0].cantidad, j.jugadores[1].vidas, j.jugadores[1].cantidad);
+    }
+}
+
+void dibujarMapa(const Juego &j, bool modoUnJugador) {
     erase();
 
     for (int y = 0; y < j.mapa.alto; y++) {
@@ -147,7 +183,7 @@ void dibujarMapa(const Juego &j) {
             mvaddch(y, x, (unsigned char)j.mapa.posiciones[y][x]);
         }
     }
-
+    interfaz_un_jugador(j, modoUnJugador);
     refresh();
 };
 
@@ -209,22 +245,53 @@ void marcarExplosion(Juego &j, int x, int y) {
     j.mapa.posiciones[y][x] = '*';
 }
 
+int distManhattan(int x1,int y1,int x2,int y2){
+    return abs(x1-x2) + abs(y1-y2);
+}
+
+bool esCeldaLibreParaPuerta(const Juego& j, int x, int y) {
+    if (x <= 0 || x >= j.mapa.largo - 1 || y <= 0 || y >= j.mapa.alto - 1) return false;
+    return j.mapa.posiciones[y][x] == ' ';
+}
+
+void colocarPuertaLejosDelSpawn(Juego& j, int spawnX, int spawnY) {
+    const int minDist = 20;
+
+    // intentos random
+    for (int t = 0; t < 2000; t++) {
+        int x = randLargo(gen);
+        int y = randAlto(gen);  
+
+        if (!esCeldaLibreParaPuerta(j, x, y)) continue;
+        if (distManhattan(spawnX, spawnY, x, y) < minDist) continue;
+
+        j.puerta = {x, y, true};
+        j.mapa.posiciones[y][x] = 'X';
+        return;
+    }
+    int fx = j.mapa.largo - 2;
+    int fy = j.mapa.alto - 2;
+    if (j.mapa.posiciones[fy][fx] == ' ') {
+        j.puerta = {fx, fy, true};
+        j.mapa.posiciones[fy][fx] = 'X';
+    }
+}
+
 //Expande la explosion de la bomba hacia arriba, abajo, izquierda y derecha
 void explotarBomba(Juego &j, Bomba bomba) {
     marcarExplosion(j, bomba.x, bomba.y);
 
     for (int i = 1; i <= bomba.distancia; i++) {
 
-        // derecha
-        if (j.mapa.posiciones[bomba.y][bomba.x + i] != '=') {
-
+            // derecha
             if (j.mapa.posiciones[bomba.y][bomba.x + i] == '#') {
                 j.mapa.posiciones[bomba.y][bomba.x + i] = '*';
+                if (bomba.autor < j.jugadores.size())
+                    j.jugadores[bomba.autor].cantidad += 10; // o .puntaje si cambias el nombre
             }
             else {
                 marcarExplosion(j, bomba.x + i, bomba.y);
             }
-        }
 
         // izquierda
         if (j.mapa.posiciones[bomba.y][bomba.x - i] != '=') {
@@ -240,19 +307,23 @@ void explotarBomba(Juego &j, Bomba bomba) {
         // abajo
         if (j.mapa.posiciones[bomba.y + i][bomba.x] != '=') {
 
-            if (j.mapa.posiciones[bomba.y + i][bomba.x] == '#') {
-                j.mapa.posiciones[bomba.y + i][bomba.x] = '*';
+                if (j.mapa.posiciones[bomba.y + i][bomba.x] == '#') {
+                    j.mapa.posiciones[bomba.y + i][bomba.x] = '*';
+                    if (bomba.autor < j.jugadores.size())
+                        j.jugadores[bomba.autor].cantidad += 10;
+                }
             }
             else {
                 marcarExplosion(j, bomba.x, bomba.y + i);
             }
-        }
 
         // arriba
         if (j.mapa.posiciones[bomba.y - i][bomba.x] != '=') {
 
             if (j.mapa.posiciones[bomba.y - i][bomba.x] == '#') {
                 j.mapa.posiciones[bomba.y - i][bomba.x] = '*';
+                if (bomba.autor < j.jugadores.size())
+                    j.jugadores[bomba.autor].cantidad += 10;
             }
             else {
                 marcarExplosion(j, bomba.x, bomba.y - i);
@@ -291,6 +362,15 @@ void* hiloBomba(void* arg) {
     eliminarBomba(*datos->juego, datos->bomba);
     explotarBomba(*datos->juego, datos->bomba);
 
+    // Quitar vida si algún jugador está sobre una explosión
+    for (int i = 0; i < datos->juego->jugadores.size(); ++i) {
+        int px = datos->juego->jugadores[i].x;
+        int py = datos->juego->jugadores[i].y;
+        if (datos->juego->mapa.posiciones[py][px] == '*') {
+            datos->juego->jugadores[i].vidas--;
+        }
+    }
+
     pthread_mutex_unlock(&mutex);
 
     usleep(700000);
@@ -313,6 +393,7 @@ void colocarBomba(Juego &j, int jugador) {
     bomba.x = j.jugadores[jugador].x;
     bomba.y = j.jugadores[jugador].y;
     bomba.distancia = 1;
+    bomba.autor = jugador;
 
     j.bombas.push_back(bomba);
     j.mapa.posiciones[bomba.y][bomba.x] = 'O'; // caracter ASCII O
@@ -326,6 +407,42 @@ void colocarBomba(Juego &j, int jugador) {
     pthread_detach(hilo);
 
     pthread_mutex_unlock(&mutex);
+}
+
+void limpiarEstadoNivel(Juego& j) {
+    j.bombas.clear();
+    j.enemigos.clear();
+    j.muros.clear();
+    j.puerta = {0, 0, false};
+}
+
+void iniciarNivelUnJugador(Juego& j) {
+    limpiarEstadoNivel(j);
+
+    // (re)generar mapa
+    inicializarMapa(j);
+
+    // (re)colocar jugador 1 en spawn fijo
+    j.jugadores.clear();
+    inicializarJugadores(j, true);
+    // Solo usas jugador 0 en este modo
+    j.mapa.posiciones[j.jugadores[0].y][j.jugadores[0].x] = '@';
+
+    // puerta lejos del spawn
+    colocarPuertaLejosDelSpawn(j, j.jugadores[0].x, j.jugadores[0].y);
+}
+
+void inicializarEnemigos(Juego& j, int cuantos) {
+    for (int e = 0; e < cuantos; ++e) {
+        int x, y;
+        do {
+            x = randLargo(gen);
+            y = randAlto(gen);
+        } while(j.mapa.posiciones[y][x] != ' '); 
+        Enemigo enemy = {"Enemigo", 1, x, y };
+        j.enemigos.push_back(enemy);
+        j.mapa.posiciones[y][x] = 'E';
+    }
 }
 
 int main() {
@@ -382,11 +499,13 @@ int main() {
             case 'U': // Un jugador
                 // Inicializar el mapa y los jugadores
                 inicializarMapa(bomberman);
-                inicializarJugadores(bomberman);
+                inicializarJugadores(bomberman, true);
+                inicializarEnemigos(bomberman, bomberman.nivel); // Agrega segun el nivel
                 bomberman.mapa.posiciones[bomberman.jugadores[0].y][bomberman.jugadores[0].x] = '@';
                 timeout(50);
                 while(jugando) {
-                    dibujarMapa(bomberman);
+                    // Si es un modo jugador, dibuja la interfaz de un jugador
+                    dibujarMapa(bomberman, true);
 
                     int ch = getch(); //Detecta la tecla presionada
                     switch(ch) {
@@ -403,14 +522,14 @@ int main() {
             case 'D': // Dos jugadores
                 // Inicializar el mapa y los jugadores
                 inicializarMapa(bomberman);
-                inicializarJugadores(bomberman);
+                inicializarJugadores(bomberman, false);
                 bomberman.mapa.posiciones[bomberman.jugadores[0].y][bomberman.jugadores[0].x] = '@';
                 bomberman.mapa.posiciones[bomberman.jugadores[1].y][bomberman.jugadores[1].x] = '@';
 
                 // Bucle principal del juego
                 timeout(50);
                 while(jugando) {
-                    dibujarMapa(bomberman);
+                    dibujarMapa(bomberman, false);
 
                     int ch = getch(); //Detecta la tecla presionada
                     switch(ch) {
